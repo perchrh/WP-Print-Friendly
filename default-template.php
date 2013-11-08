5cfcd15b73161d7d49bba564a827b2ecd9547b33
Per Christian Henden <perchrh-gh@pvv.org>
2013-11-08 14:21:58 +0100

    Localize for Norway

--- default-template.php
+++ default-template.php
@@ -14,23 +14,23 @@
 				?>
 				<div <?php post_class(); ?>>
 					<h1><?php the_title(); ?></h1>
-					<p>by <?php the_author(); ?> | <?php the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></p>
+					<p>by <?php bloginfo( 'name' ); ?>| <?php the_time( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) ); ?></p>
 
 					<?php
 						if( is_attachment() && wp_attachment_is_image() )
 							echo '<p>' . wp_get_attachment_image( $post->ID, 'large' ) . '</p>';
 
-						add_filter( 'the_content', 'remove_images', 100 );
-						add_filter( 'the_content', 'remove_image_captions', 101 );
+					add_filter( 'the_content', 'remove_images', 100 );
+					add_filter( 'the_content', 'remove_image_captions', 101 );
 						the_content();
 					?>
 
 					<?php
 						if( function_exists( 'wpf_the_page_numbers' ) )
-							wpf_the_page_numbers( false, '<p class="page_numbers">Page ', ' of ', '</p><!-- .page_numbers -->' );
+							wpf_the_page_numbers( false, '<p class="page_numbers">Side ', ' av ', '</p><!-- .page_numbers -->' );
 					?>
 
-					<p class="wpf-source"><strong>Source URL:</strong> <?php the_permalink(); ?></p>
+					<p class="wpf-source"><strong>Kilde:</strong> <?php the_permalink(); ?></p>
 
 					<hr class="wpf-divider" />
 				</div>
@@ -38,8 +38,5 @@
 			endwhile;
 		endif;
 	?>
-
-		<p class="copyright">Copyright &copy;<?php echo date( 'Y' ); ?> <strong><?php bloginfo( 'name' ); ?></strong> unless otherwise noted.</p>
-
 	</body>
 </html>
